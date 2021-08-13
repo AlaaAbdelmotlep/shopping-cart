da4459f11410687c4c9b24c1dbb551e259b90a43
AlaaAbdelmotlep <52351170+AlaaAbdelmotlep@users.noreply.github.com>
2021-08-13 17:04:37 +0200

    test commit

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,17 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class NavBar extends Component {
-    state = {  }
-    render() { 
-        return ( 
-            <React.Fragment>
-                <nav className='navbar navbar-dark bg-dark'>
-                    <a className='navbar-brand' href='#'>NavBar</a>
-                    <span className='badge badge-primary'>{this.props.productCount}</span>
-                </nav>
-            </React.Fragment>
-         );
-    }
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <nav className="navbar navbar-dark bg-dark">
+          <a className="navbar-brand" href="#">
+            NavBar
+          </a>
+          <span className="badge badge-primary">{this.props.productCount}</span>
+        </nav>
+      </React.Fragment>
+    );
+  }
 }
- 
+
 export default NavBar;
